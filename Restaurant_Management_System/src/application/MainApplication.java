97697a6a18d09b786761666d9f5370a8bda4a005
devyani-14 <64266292+devyani-14@users.noreply.github.com>
2024-12-10 17:19:42 -0500

    UI changes: 1

--- Restaurant_Management_System/src/application/MainApplication.java
+++ Restaurant_Management_System/src/application/MainApplication.java
@@ -1,27 +1,32 @@
 package application;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainApplication extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception {
-    	
-    	try {
-        // Load the customer interface
-    	FXMLLoader loader = new FXMLLoader(MainApplication.class.getResource("/customer_interface.fxml"));
-        Region root = loader.load();
-
-        // Apply CSS styling
-        root.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
-
-        primaryStage.setTitle("Restaurant Management System");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
-    	} catch (Exception e) {
-            e.printStackTrace(); // Print the error
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/customer_interface.fxml"));
+            VBox root = loader.load();
+            
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
+            
+            // Set fixed window size
+            primaryStage.setWidth(800);
+            primaryStage.setHeight(600);
+            primaryStage.setResizable(false);  // Make window non-resizable
+            
+            primaryStage.setTitle("CaféDine Restaurant");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
