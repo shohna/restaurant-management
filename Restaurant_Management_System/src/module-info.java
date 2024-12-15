979cefb276ba7f822b59874b0e119ff10fa4ba0a
devyani-14 <64266292+devyani-14@users.noreply.github.com>
2024-12-14 19:15:03 -0500

    Revert "Added class files"
    This reverts commit 8ed796e760b98988611463bbce287283959ff41e.

--- Restaurant_Management_System/src/module-info.java
+++ Restaurant_Management_System/src/module-info.java
@@ -2,9 +2,14 @@ module Restaurant_Management_System {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
-    requires java.sql;
+    requires java.sql;  
 
     opens application to javafx.fxml;
+<<<<<<< HEAD
     opens resources to javafx.fxml;
+=======
+    opens resources to javafx.fxml; // Add this line
+    
+>>>>>>> parent of 8ed796e (Added class files)
     exports application;
 }
