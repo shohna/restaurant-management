f8039dfdbfb9026947aee4ed12e7f2a0fe6f1d77
sarasap <sarasa.pattabiraman@gmail.com>
2024-12-15 18:34:37 -0500

    deleting repo

--- Restaurant_Management_System/src/application/DatabaseConnection.java
+++
@@ -1,7 +0,0 @@
-package application;
-public class DatabaseConnection {
-    // Placeholder for future database operations
-    public DatabaseConnection() {
-        System.out.println("Database connection initialized.");
-    }
-}
--- Restaurant_Management_System/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module Restaurant_Management_System {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-
-    opens application to javafx.fxml;  // Allow JavaFX to access the application's package
-
-    exports application;
-}
